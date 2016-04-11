1b438555ce7bf4b46bf55491c0000b5f59732b75
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-11 15:08:30 -0500

    changed the process to follow the compile + run convention instead of make + run

--- svcomp_runner.py
+++ svcomp_runner.py
@@ -3,69 +3,97 @@ import os
 import subprocess32 as subprocess
 import re
 
-
-
-def find_undef_behavior(command):
-    sys.stdout.write(" ".join(command) + " --- ")
-    sys.stdout.flush()
-    error_regex = re.compile('(UB|CV|USP)\-([A-Z]+[0-9]*)')
-    try:
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        process.wait(timeout=1)
-        output = process.stderr.read()
-        if re.search(error_regex, output):
-            return True, output
-        return False, ""
-    except subprocess.TimeoutExpired:
-        return False, ""
-    finally:
-        process.kill()
-
-
+total_tests = 0
+total_undefined = 0
 location = sys.argv[1]
 log_location = sys.argv[2]
-tests_run = 0
-undefined = 0
+includes_location = os.path.join(location, "decls.h")
+implementation_location = os.path.join(location, "implementations.c")
+
+
+def check_result(output):
+    error_regex = re.compile('(UB|CV|USP)\-([A-Z]+[0-9]*)')
+    if re.search(error_regex, output):
+        return True, output
+    return False, ""
+
 
 def log_result(file, executable, result, output):
-    file.write("Executable " + executable + "\n")
+    file.write("File " + executable + "\n")
     if result:
         sys.stdout.write("UNDEFINED!\n")
         file.write("Found Undefined Behavior! \n")
         file.write(output + "\n")
     else:
         sys.stdout.write("OK!\n")
-        file.write("Executable was well defined \n")
+        file.write("File was well defined \n")
     file.write("\n")
 
 
+def run_command(command, timeout):
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.wait(timeout=timeout)
+        output = subprocess.stderr.read()
+        return check_result(output)
+    except subprocess.TimeoutExpired:
+        return False, ""
+    finally:
+        process.kill()
+
+
 def run_example(example_folder):
-    global tests_run
-    global undefined
-    os.chdir(os.path.join(location, "bin", example_folder))
+    tests_run = 0
+    undefined = 0
+    global total_tests
+    global total_undefined
+    os.chdir(os.path.join(location, example_folder))
     print("In Directory - " + os.getcwd())
     output_file = open(os.path.join(log_location, example_folder + "-results.txt", "w+"))
-    correct_count = 0
-    undef_count = 0
     for file in os.listdir(os.getcwd()):
-        if "true" in file and file.endswith(".oc"):
-            correct_count += 1
-            tests_run = tests_run + 1
-            undefined = undefined + 1
-            result, output = find_undef_behavior(["./" + file])
-            log_result(output_file, file, result, output)
+        if "true" in file and file.endswith(".c"):
+            sys.stdout.write("Compiling File " + file + " -- ")
+            sys.stdout.flush()
+            tests_run += 1
+            # compile the file
+            executable_name = file.split(".c")[0] + ".out"
+            result, output = run_command(
+                    ["kcc", "-include", includes_location, file, implementation_location, "-o", executable_name], 10)
             if result:
-                undef_count += 1
-    output_file.write("Total Executables - " + str(correct_count) + "\n")
-    output_file.write("Undefined - " + str(undef_count) + "\n")
+                sys.stdout.write("UNDEFINED! \n");
+                sys.stdout.flush()
+                log_result(output_file, file, result, output)
+                undefined += 1
+                continue
+            else:
+                # run the executable
+                sys.stdout.write("OK! \n");
+                sys.stdout.flush()
+                sys.stdout.write("Running " + executable_name + " -- ")
+                sys.stdout.flush()
+                result, output = run_command(["./", executable_name, 2])
+                log_result(output_file, file, result, output)
+                if result:
+                    sys.stdout.write("UNDEFINED!\n")
+                    sys.stdout.flush()
+                    undefined += 1
+                else:
+                    sys.stdout.write("OK!\n")
+                    sys.stdout.flush()
+
+    output_file.write("Total Executables - " + str(tests_run) + "\n")
+    output_file.write("Undefined - " + str(undefined) + "\n")
     output_file.close()
-
-
+    total_tests += tests_run
+    undefined += undefined
 
 
 def main():
     test_file = open("tests.txt", 'r')
-    map(lambda y: run_example(y), filter(lambda x : x.split(), test_file.split("\n")))
+    map(lambda y: run_example(y), filter(lambda x: x.split(), test_file.split("\n")))
+    print(str(total_undefined))
+    print(str())
+
 
 if __name__ == '__main__':
     main()
