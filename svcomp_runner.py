21b0f51a59ad6442f3005b83c71a659736f74b97
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-11 15:35:00 -0500

    process of compiling + running in one pass working

--- svcomp_runner.py
+++ svcomp_runner.py
@@ -34,12 +34,12 @@ def run_command(command, timeout):
     try:
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process.wait(timeout=timeout)
-        output = subprocess.stderr.read()
+        output = process.stderr.read()
+        process.kill()
         return check_result(output)
     except subprocess.TimeoutExpired:
-        return False, ""
-    finally:
         process.kill()
+        return False, ""
 
 
 def run_example(example_folder):
@@ -49,10 +49,10 @@ def run_example(example_folder):
     global total_undefined
     os.chdir(os.path.join(location, example_folder))
     print("In Directory - " + os.getcwd())
-    output_file = open(os.path.join(log_location, example_folder + "-results.txt", "w+"))
+    output_file = open(os.path.join(log_location, example_folder + "-results.txt"), "w+")
     for file in os.listdir(os.getcwd()):
         if "true" in file and file.endswith(".c"):
-            sys.stdout.write("Compiling File " + file + " -- ")
+            sys.stdout.write("---> Compiling File " + file + " -- ")
             sys.stdout.flush()
             tests_run += 1
             # compile the file
@@ -60,27 +60,20 @@ def run_example(example_folder):
             result, output = run_command(
                     ["kcc", "-include", includes_location, file, implementation_location, "-o", executable_name], 10)
             if result:
-                sys.stdout.write("UNDEFINED! \n");
-                sys.stdout.flush()
                 log_result(output_file, file, result, output)
                 undefined += 1
                 continue
             else:
+                sys.stdout.write("OK!\n")
+                sys.stdout.flush()
                 # run the executable
-                sys.stdout.write("OK! \n");
+                sys.stdout.write("--> Running " + executable_name + " -- ")
                 sys.stdout.flush()
-                sys.stdout.write("Running " + executable_name + " -- ")
-                sys.stdout.flush()
-                result, output = run_command(["./", executable_name, 2])
+                result, output = run_command(["./" + executable_name], 2)
                 log_result(output_file, file, result, output)
                 if result:
-                    sys.stdout.write("UNDEFINED!\n")
-                    sys.stdout.flush()
                     undefined += 1
-                else:
-                    sys.stdout.write("OK!\n")
-                    sys.stdout.flush()
-
+            os.remove(executable_name)
     output_file.write("Total Executables - " + str(tests_run) + "\n")
     output_file.write("Undefined - " + str(undefined) + "\n")
     output_file.close()
@@ -89,10 +82,10 @@ def run_example(example_folder):
 
 
 def main():
-    test_file = open("tests.txt", 'r')
+    test_file = open("tests.txt", 'r').read()
     map(lambda y: run_example(y), filter(lambda x: x.split(), test_file.split("\n")))
-    print(str(total_undefined))
-    print(str())
+    print("Total Undefined - " + str(total_undefined))
+    print("Total Run - " + str())
 
 
 if __name__ == '__main__':
