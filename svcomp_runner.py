d2fc1ef0a5cba86e08b4c17440fcceeaf37cfb64
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-11 13:55:50 -0500

    added support for automatic test running

--- svcomp_runner.py
+++ svcomp_runner.py
@@ -12,7 +12,7 @@ def find_undef_behavior(command):
     try:
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process.wait(timeout=1)
-        output = process.stdout.read() + process.stderr.read()
+        output = process.stderr.read()
         if re.search(error_regex, output):
             return True, output
         return False, ""
@@ -23,7 +23,9 @@ def find_undef_behavior(command):
 
 
 location = sys.argv[1]
-
+log_location = sys.argv[2]
+tests_run = 0
+undefined = 0
 
 def log_result(file, executable, result, output):
     file.write("Executable " + executable + "\n")
@@ -38,14 +40,18 @@ def log_result(file, executable, result, output):
 
 
 def run_example(example_folder):
+    global tests_run
+    global undefined
     os.chdir(os.path.join(location, "bin", example_folder))
     print("In Directory - " + os.getcwd())
-    output_file = open("results.txt", "w+")
+    output_file = open(os.path.join(log_location, example_folder + "-results.txt", "w+"))
     correct_count = 0
     undef_count = 0
     for file in os.listdir(os.getcwd()):
         if "true" in file and file.endswith(".oc"):
             correct_count += 1
+            tests_run = tests_run + 1
+            undefined = undefined + 1
             result, output = find_undef_behavior(["./" + file])
             log_result(output_file, file, result, output)
             if result:
@@ -58,31 +64,8 @@ def run_example(example_folder):
 
 
 def main():
-    #run_example("array-examples")
-    #run_example("bitvector")
-    #run_example("bitvector-regression")
-    #run_example("bitvector-loops")
-    #run_example("signedintegeroverflow-regression")
-    #run_example("list-properties")
-    #run_example("ldv-regression")
-    #run_example("ddv-machzwd")
-    #run_example("list-ext-properties")
-    #run_example("ldv-memsafety")
-    #run_example("floats-cdfpl")
-    #run_example("float-benchs")
-    #run_example("ssh-simplified")
-    #run_example("locks")
-    #run_example("loop-acceleration")
-    #run_example("loop-invgen")
-    #run_example("loop-lit")
-    #run_example("systemc")
-    #run_example("termination-crafted")
-    #run_example("termination-numeric")
-    #run_example("termination-restricted-15")
-    #run_example("pthread")
-    #run_example("pthread-ext")
-    #run_example("pthread-wmm")
-    run_example("pthread-lit")
+    test_file = open("tests.txt", 'r')
+    map(lambda y: run_example(y), filter(lambda x : x.split(), test_file.split("\n")))
 
 if __name__ == '__main__':
     main()
