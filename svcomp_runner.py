88244b48e70c272bb401a6434d51f3e5b77ad613
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-13 18:17:39 -0500

    changed result files + added logs

--- svcomp_runner.py
+++ svcomp_runner.py
@@ -4,13 +4,12 @@ import signal
 import subprocess32 as subprocess
 import re
 
-total_tests = 0
-total_undefined = 0
+
 location = sys.argv[1]
 log_location = sys.argv[2]
 includes_location = os.path.join(location, "decls.h")
 implementation_location = os.path.join(location, "implementations.o")
-implementation2_location = os.path.join(location, "implementations2.o")
+
 
 def check_result(output):
     error_regex = re.compile('(UB|CV|USP)\-([A-Z]+[0-9]*)')
@@ -49,11 +48,10 @@ def run_command(command, timeout):
         except OSError:
             pass
 
+
 def run_example(example_folder):
     tests_run = 0
     undefined = 0
-    global total_tests
-    global total_undefined
     os.chdir(os.path.join(location, example_folder))
     print("In Directory - " + os.getcwd())
     output_file = open(os.path.join(log_location, example_folder + "-results.txt"), "w+")
@@ -114,8 +112,6 @@ def run_example(example_folder):
     output_file.write("Total Executables - " + str(tests_run) + "\n")
     output_file.write("Undefined - " + str(undefined) + "\n")
     output_file.close()
-    total_tests += tests_run
-    total_undefined += undefined
 
 
 def init_implementations():
@@ -126,13 +122,37 @@ def init_implementations():
     sys.stdout.write("ok\n")
     sys.stdout.flush()
 
+def tabulate_results(test_folders):
+    print "length" + str(len(test_folders))
+    undef_regex = re.compile('Undefined\s\-\s([0-9]+)')
+    run_regex = re.compile('Executables\s\-\s([0-9]+)')
+    total_undef = 0
+    total_run = 0
+    for folder in test_folders:
+        try:
+            file = open(os.path.join(log_location, folder + "-results.txt"))
+            content = file.read()
+            undef = undef_regex.search(content)
+            run = run_regex.search(content)
+            if undef is not None and run is not None:
+                total_undef += int(undef.group(1))
+                total_run += int(run.group(1))
+        except IOError as e:
+            print e.message
+    print "Number of Correct Files Run (with kcc) - " + str(total_run)
+    print "Total Number of Correct Files with undefined behavior - " + str(total_undef)
+
+
+
+
+
 
 def main():
     test_file = open("tests.txt", 'r').read()
-    init_implementations()
-    map(lambda y: run_example(y), filter(lambda x: x.split(), test_file.split("\n")))
-    print("Total Undefined - " + str(total_undefined))
-    print("Total Run - " + str(total_tests))
+    # init_implementations()
+    test_folders = filter(lambda x: x.split(), test_file.split("\n"))
+    # map(lambda y: run_example(y), test_folders)
+    tabulate_results(test_folders)
 
 
 if __name__ == '__main__':
